2938b6496ba8126503121d6c92eeaff369daaad8
GeloPC <michaelangelosdr@gmail.com>
2018-01-20 23:41:18 +0800

    Added Factory Method for object instantiating.

---
+++ Easter_Egg_App/Assets/GM_Script.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		ObjectFactory.TryCreateObject (Egg_Type.BASIC);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
