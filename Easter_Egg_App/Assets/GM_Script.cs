c2998f629dd6138f31e19d574d5985b204c907b9
GeloPC <michaelangelosdr@gmail.com>
2018-01-25 01:48:00 +0800

    added egg summoning

--- Easter_Egg_App/Assets/GM_Script.cs
+++ Easter_Egg_App/Assets/GM_Script.cs
@@ -1,18 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GM_Script : MonoBehaviour {
 
+	[SerializeField] Transform TestTarget_list;
+	private List<Transform> AllTargets;
+	public int Number_Of_Targets;
+	private int[] Image_Targets;
+
+	private Transform[] targets;
+	//private List<GameObject> Targets = GetComponentInChildren<Transform>();
+
+
+
 	// Use this for initialization
 	void Start () {
 
-		ObjectFactory.TryCreateObject (Egg_Type.BASIC);
+		Image_Targets = new int[Number_Of_Targets];
+		
+		AllTargets = new List<Transform> ();
+
+		foreach (Transform target in TestTarget_list) {
+			AllTargets.Add (target);
+		}
+
+	
+
+		for (int x = 0; x < Number_Of_Targets; x++) {
+			Image_Targets [x] = Random.Range (0, AllTargets.Count);	
+		}
+
+
+		
+
+		StartCoroutine (LoadEggs ());
+
+
+
+	
+	
+	}
+
+	public void SpawnEggsOnTargets()
+	{
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+		
+
+	IEnumerator LoadEggs()
+	{
+		for (int x = 0; x < Number_Of_Targets; x++) {
+			
+			BaseObject G; 
+			G =	ObjectFactory.TryCreateObject (Egg_Type.BASIC);
+			G.name = "BasicEgg";
+			G.transform.SetParent (AllTargets[Image_Targets[x]].transform);
+			G.transform.localPosition = new Vector3 (0, 0.25f, -0.25f);
+
+		}
+		
+		yield return new WaitForEndOfFrame ();
+
 	}
 }
