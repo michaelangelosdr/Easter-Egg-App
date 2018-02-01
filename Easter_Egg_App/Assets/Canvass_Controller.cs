afb374cf352659e80f997bc208cbfa1f443658d6
GeloPC <michaelangelosdr@gmail.com>
2018-02-01 23:15:49 +0800

    Added functionality to Egg _ app Functionality tracking 60%

---
+++ Easter_Egg_App/Assets/Canvass_Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class Canvass_Controller : MonoBehaviour {
+
+
+	[SerializeField] List<GameObject> Object_Collect_Images;
+	[SerializeField] Text Object_Count_Text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	public void Object_Visibility_On(int Object_Index)
+	{
+		if(Object_Collect_Images [Object_Index].GetComponent<Image> ().enabled == false)
+		Object_Collect_Images [Object_Index].GetComponent<Image> ().enabled = true;
+	}
+
+	public void Object_Visibility_Off(int Object_Index)
+	{
+		if(Object_Collect_Images [Object_Index].GetComponent<Image> ().enabled == true)
+			Object_Collect_Images [Object_Index].GetComponent<Image> ().enabled = false;
+	}
+		
+	public void Object_Count_Change()
+	{
+
+		Object_Count_Text.text = GM_Script.Object_Collected_Count.ToString();
+	}
+
+}
--- Easter_Egg_App/Assets/GM_Script.cs
+++ Easter_Egg_App/Assets/GM_Script.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GM_Script : MonoBehaviour {
 
 	[SerializeField] Transform TestTarget_list;
+	[SerializeField]  Canvass_Controller C_controller;
+
 	private List<Transform> AllTargets;
 	public int Number_Of_Targets;
 	private int[] Image_Targets;
 
+
+	public static int Object_Collected_Count;
+
+
+
+
 	private Transform[] targets;
+
+
 	//private List<GameObject> Targets = GetComponentInChildren<Transform>();
 
 
@@ -18,9 +29,19 @@ public class GM_Script : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
+
+		Object_Collected_Count = 0;
+
 		Image_Targets = new int[Number_Of_Targets];
 		
 		AllTargets = new List<Transform> ();
+
+		if (Object_Collected_Count <= 0) {
+			for (int x = 0; x < 5; x++) {	
+				C_controller.Object_Visibility_Off (x);
+			}
+		}
+
 
 		foreach (Transform target in TestTarget_list) {
 			AllTargets.Add (target);
@@ -32,7 +53,29 @@ public class GM_Script : MonoBehaviour {
 			Image_Targets [x] = Random.Range (0, AllTargets.Count);	
 		}
 
+	
+		for (int x = 0; x < Number_Of_Targets; x++) {
 
+			for (int y = x + 1; y < Number_Of_Targets ; y++) {
+
+				if(Image_Targets[x] == Image_Targets[y])
+				{
+					Debug.Log (Image_Targets [x] + "AND" + Image_Targets[y] );
+				}
+
+				while (Image_Targets [x] == Image_Targets [y]) {
+					Image_Targets [x] = Random.Range (0, AllTargets.Count);	
+				}
+
+
+
+			}
+
+
+		}
+
+
+		
 		
 
 		StartCoroutine (LoadEggs ());
@@ -47,15 +90,19 @@ public class GM_Script : MonoBehaviour {
 	{
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 		
 
 	IEnumerator LoadEggs()
 	{
+
+
+
+
 		for (int x = 0; x < Number_Of_Targets; x++) {
 			
 			BaseObject G; 
@@ -64,9 +111,29 @@ public class GM_Script : MonoBehaviour {
 			G.transform.SetParent (AllTargets[Image_Targets[x]].transform);
 			G.transform.localPosition = new Vector3 (0, 0.25f, -0.25f);
 
+			if (x == Number_Of_Targets) {
+				G.name = "Last";
+				G.GetComponent<MeshRenderer> ().enabled = false;
+				G.GetComponent<MeshCollider> ().enabled = false;
+			}
+
 		}
 		
 		yield return new WaitForEndOfFrame ();
 
 	}
+
+
+	public void Object_Collected_Successfully()
+	{
+		C_controller.Object_Visibility_On (Object_Collected_Count);	
+		Debug.Log ("Egg_Collected");
+		Object_Collected_Count++;
+		C_controller.Object_Count_Change ();
+	}
+
+
+	// ======================= Canvas Manager ============================
+
+
 }
