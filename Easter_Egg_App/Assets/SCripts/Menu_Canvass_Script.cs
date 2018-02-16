a945408653a8c671e77ecfbf48e2fc2461cced19
GeloPC <michaelangelosdr@gmail.com>
2018-02-16 17:10:56 +0800

    Added Main Menu to project
    + Added Egg functionality

---
+++ Easter_Egg_App/Assets/SCripts/Menu_Canvass_Script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu_Canvass_Script : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	public void ButtonFunctionality(GameObject Button)
+	{
+		if (Button.name == "start_but") {
+			//Load Game scene
+			SceneManager.LoadScene (1);
+		}
+
+
+
+	}
+
+}
